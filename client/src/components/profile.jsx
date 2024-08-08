152dc633104deba591c1663e6a4162cd89220576
zachfrosty4986 <zachjames0114@gmail.com>
2024-08-08 18:54:51 -0400

    Moved front end files to proper location

---
+++ client/src/components/profile.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import './styles.css';
+
+const Profile = ({ username }) => {
+    return (
+        <div>
+            {/* Include header */}
+            <div id="header">
+                {/* Include header.html */}
+                {/* Add your header content here */}
+            </div>
+
+            {/* Main content */}
+            <div id="main-content">
+                <h1>Welcome {username}</h1>
+
+                {/* Friends box */}
+                <div id="friends-box">
+                    <h2>Friends</h2>
+                    {/* Add your friends list here */}
+                </div>
+
+                {/* Upcoming trips box */}
+                <div id="upcoming-trips-box">
+                    <h2>Upcoming Trips</h2>
+                    {/* Add your upcoming trips list here */}
+                </div>
+
+                {/* Previous trips box */}
+                <div id="previous-trips-box">
+                    <h2>Previous Trips</h2>
+                    {/* Add your previous trips list here */}
+                </div>
+
+                {/* Dream trips box */}
+                <div id="dream-trips-box">
+                    <h2>Dream Trips</h2>
+                    {/* Add your dream trips list here */}
+                </div>
+            </div>
+
+            {/* User icon box */}
+            <div id="user-icon-box">
+                {/* Add your user icon here */}
+            </div>
+
+            {/* Include footer */}
+            <div id="footer">
+                {/* Include footer.html */}
+                {/* Add your footer content here */}
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
