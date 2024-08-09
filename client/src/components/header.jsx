a1286425e8265f82817e4b1da3e0879fe1f801a2
Destiny <dejenkins@justworks.com>
2024-08-08 21:39:48 -0400

    element, format, and styling changes to header.jsc and app.css files

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 const Header = () => {
     return (
         <header>
-            <div className="logo">Nomad Notes</div>
+            <h1 className="logo">Nomad Notes</h1>
+            <nav>
             <div className="dropdown">
                 <button className="dropbtn">Explore</button>
                 <div className="dropdown-content">
-                    <a href="#">Link 1</a>
-                    <a href="#">Link 2</a>
-                    <a href="#">Link 3</a>
+                    <a className ="navlink1" href="#">Link 1</a>
+                    <a className ="navlink2" href="#">Link 2</a>
+                    <a className ="navlink3"href="#">Link 3</a>
                 </div>
             </div>
+            </nav>
         </header>
     );
 };
