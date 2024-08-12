499d143f01f7040250eb345a956252775d465c39
donnacancode <donnacancode@gmail.com>
2024-08-12 13:02:57 -0400

    updated routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,37 +1,71 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import Landing from "./pages/Landing";
+import LandingPage from "./gb-front-end/pages/LandingPage";
+import DreamTrips from "./pages/dreamTrips.jsx";
+import IndividualTrip from "./pages/individualTrip.jsx";
+import Login from "./pages/login.jsx";
+import PreviousTrips from "./pages/previousTrips.jsx";
+import Profile from "./pages/profile.jsx";
+import Signup from "./pages/signup.jsx";
+import UpcomingTrips from "./pages/upcomingTrips.jsx";
+import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <h1>Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "/altlanding",
+        element: <LandingPage />,
+      },
+      {
+        path: "/dreamtrips",
+        element: <DreamTrips />,
+      },
+      {
+        path: "/individualtrip",
+        element: <IndividualTrip />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/previoustrips",
+        element: <PreviousTrips />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/upcomingtrips",
+        element: <UpcomingTrips />,
+      },
+    ],
+  },
+]);
 
-
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <App />,
-//     errorElement: <h1>Wrong page!</h1>,
-//     children: [
-//       {
-//         index: true,
-//         element: <Landing />
-//       } 
-//       {
-//         path: '/putpathinhere',
-//         element: < />
-//       }
-//     ]
-//   }
-// ])
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <RouterProvider router={router} />
-// )
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
 
 // Will delete the lines below eventually
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
