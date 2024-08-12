eb31bae4a0e690ce09be6cfba473dcdbce1997b6
donnacancode <donnacancode@gmail.com>
2024-08-12 13:01:55 -0400

    updated to use auth and proper model importation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,53 +1,53 @@
-import { AuthenticationError } from 'apollo-server-express';
-import userModel from './models/userModel';
-import { signToken } from './utils/auth';
+const { User, Trip } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     // Query to fetch all users
     users: async () => {
-      return await userModel.find({});
+      return await User.find({});
     },
     // Query to fetch a single user by ID
     user: async (_, { id }) => {
-      return await userModel.findById(id);
+      return await User.findById(id);
     },
     // Query to fetch the currently authenticated user
     me: async (_, __, context) => {
       if (context.user) {
-        return await userModel
-          .findById(context.user._id)
-          .select("-__v -password");
+        return await User.findById(context.user._id).select("-__v -password");
       }
-      throw new AuthenticationError("Not logged in");
+      throw AuthenticationError;
     },
   },
 
   Mutation: {
     // Mutation for adding a new user
     addUser: async (_, { username, email, password }) => {
-      const user = await userModel.create({ username, email, password });
+      const user = await User.create({ username, email, password });
       const token = signToken(user);
       return { token, user };
     },
     // Mutation for logging in an existing user
     loginUser: async (_, { email, password }) => {
-      const user = await userModel.findOne({ email });
+      const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError("No user found with this email address");
+        throw AuthenticationError;
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError("Incorrect credentials");
+        throw AuthenticationError;
       }
 
       const token = signToken(user);
       return { token, user };
     },
+
+    addComment: async (_, { commentText }, context) => {},
+    removeComment: async (_, { commentId }, context) => {},
   },
 };
 
-export default resolvers;
+module.exports = resolvers;
