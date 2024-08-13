238be2d8bc5803a77dd30ab3375075bd1a46e821
VPVelthuizen <vpvelthuizen@gmail.com>
2024-08-13 13:44:11 -0400

    updated pages folder

--- client/src/pages/landing/landing.jsx
+++ client/src/pages/landing/landing.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import useState from "react";
 import { Link } from "react-router-dom";
-import Header from "../components/header";
-import Footer from "../components/footer";
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+import { useQuery } from "@apollo/client";
+import { GET_ME } from "../../utils/queries";
+import "./landing.css";
 
 const Landing = () => {
+
   return (
     <div>
       <header>
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -32,9 +32,10 @@ class AuthService {
     return localStorage.getItem('id_token');
   }
 
-  login(idToken) {
+  login(idToken, user) {
     // Saves user token to localStorage
     localStorage.setItem('id_token', idToken);
+    localStorage.setItem('user', JSON.stringify(user));
     window.location.assign('/');
   }
 
