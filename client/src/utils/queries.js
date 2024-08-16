f65d95b96907eda79675a0fbc5729685e3f538ec
Greg Bailey <gregbailey617@gmail.com>
2024-08-15 23:31:35 -0400

    still getting error

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,39 +1,29 @@
-import React from 'react'; // Import React library
-import Header from '../../components/header'; // Import Header component for the page header
-import Footer from '../../components/footer'; // Import Footer component for the page footer
-import './trips.css'; // Import CSS for styling the IndividualTrip page
-// import { GET_TRIP } from '../../graphql/queries'; // Import GraphQL query for fetching trip data
-import Trip from '../../components/trip'; // Import Trip component to display individual trip details
+import { gql } from '@apollo/client';  
 
-const IndividualTrip = ({ username }) => {
-    // Execute the GET_TRIP query to fetch the trip details
-    const { loading, error, data } = useQuery(GET_TRIP, {
-        variables: {
-            tripId: 'PLACEHOLDER_TRIP_ID' // Replace with the actual trip ID to fetch data for a specific trip
-        }
-    });
 
-    // Display loading state while data is being fetched
-    if (loading) return <p>Loading...</p>;
-    
-    // Display error message if there is an issue with the query
-    if (error) return <p>Error: {error.message}</p>;
+export const GET_USER_TRIPS = gql`
+  query getUserTrips {
+    me {
+      _id
+      username
+      trips {
+        _id
+        location
+        journalEntry
+      }
+    }
+  }
+`;
 
-    // Extract trip data from the query result
-    const trip = data.trip;
+  export const GET_TRIP = gql`
+    query GetTrip($tripId: ID!) {
+      trip(id: $tripId) {
+        _id
+        destination
 
-    return (
-        <div>
-            {/* Render the Header component */}
-            <Header />
-            
-            {/* Render the Trip component with the fetched trip data */}
-            <Trip trip={trip} />
-            
-            {/* Render the Footer component */}
-            <Footer />
-        </div>
-    );
-};
+      }
+    }
+  `;
 
-export default IndividualTrip; // Export IndividualTrip component for use in other parts of the application
+//   startTripDate
+//   endTripDate
--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -1,50 +1,49 @@
 // Define the GraphQL schema using SDL (Schema Definition Language)
 const typeDefs = `
-  # User type defines the structure of a user object
   type User {
-    _id: ID  # Unique identifier for the user
-    username: String  # Username of the user
-    email: String  # Email address of the user
-    profile: String  # Optional profile information for the user
-    trips: [Trip]  # Array of trips associated with the user
-    friends: [User]  # Array of users who are friends with this user
+    _id: ID  
+    username: String  
+    email: String  
+    profile: String  
+    trips: [Trip]  
+    friends: [User]  
   }
 
-  # Trip type defines the structure of a trip object
+
   type Trip {
-    _id: ID  # Unique identifier for the trip
-    location: String  # Location of the trip
-    journalEntry: String  # Journal entry or notes about the trip
-    comments: [Comment]  # Array of comments associated with the trip
+    _id: ID  
+    location: String  
+    journalEntry: String  
+    comments: [Comment]  
   }
 
-  # Comment type defines the structure of a comment object
+
   type Comment {
-    _id: ID  # Unique identifier for the comment
-    comment: String  # Text of the comment
-    username: String  # Username of the person who made the comment
+    _id: ID  
+    comment: String  
+    username: String  
   }
 
-  # Auth type defines the structure of the authentication response
+
   type Auth {
-    token: String  # JWT token for the authenticated user
-    user: User  # User object associated with the token
+    token: String  
+    user: User  
   }
 
-  # Query type defines the read operations available in the schema
+
   type Query {
-    users: [User]  # Query to fetch all users
-    user(id: ID!): User  # Query to fetch a single user by ID
-    me: User  # Query to fetch the currently authenticated user
+    users: [User]  
+    user(id: ID!): User  
+    me: User  
   }
 
-  # Mutation type defines the write operations available in the schema
+
   type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth  # Mutation to add a new user
-    loginUser(username: String!, password: String!): Auth  # Mutation to log in an existing user
-    addTrip(username: String!, location: String!, journalEntry: String!): User  # Mutation to add a new trip
-    addComment(commentText: String!): Comment  # Mutation to add a comment to a trip
-    removeComment(commentId: ID!): Comment  # Mutation to remove a comment by ID
+    addUser(username: String!, email: String!, password: String!): Auth  
+    loginUser(username: String!, password: String!): Auth  
+    addTrip(username: String!, location: String!, journalEntry: String!): User  
+    addComment(commentText: String!): Comment  
+    removeComment(commentId: ID!): Comment  
   }
 `;
 
