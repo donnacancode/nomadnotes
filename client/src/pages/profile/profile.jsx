da5fc2443ea8af787be1bbb848fbf71b04c03148
Greg Bailey <gregbailey617@gmail.com>
2024-08-17 13:11:58 -0400

    functionality added so the dates will go into their correct section

--- client/src/pages/profile/profile.jsx
+++ client/src/pages/profile/profile.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import Header from '../../components/header';
 import Footer from '../../components/footer';
 import AddTrip from '../../components/addTripDate';
+import UpcomingTrips from '../../components/Trips/upcomingTrips';
+import PreviousTrips from '../../components/Trips/previousTrips';
 import './profile.css';
 import northern_lights from '../../assets/northern_lights.png';
 import { Navigate, useParams } from 'react-router-dom';
@@ -15,13 +17,31 @@ import Auth from '../../utils/auth';
 const Profile = () => {
     const [loadUserTrips, { called, loading, data }] = useLazyQuery(GET_USER_TRIPS);
     // const user = data?.me || {};
-    const [user, setUser] = useState({})
+
+    const upcomingTrips = []
+    const prevTrips = []
+
+    const [user, setUser] = useState({ trips: [] })
     useEffect(() => {
         loadUserTrips();
         if (data) {
             setUser(data.me)
         }
     }, [data, user.trips])
+
+    for (const trip of user.trips) {
+        const startTripDate = new Date(trip.startTripDate).getTime();       
+
+        if (startTripDate > Date.now()) {
+            upcomingTrips.push(trip);
+        }
+        if (startTripDate < Date.now()) {
+            prevTrips.push(trip);
+        }
+    }
+
+    console.log(prevTrips)
+
 
     if (!Auth.loggedIn()) {
         return <Navigate to="/" />;
@@ -46,16 +66,21 @@ const Profile = () => {
                     {/* Upcoming trips box */}
                     <div id="upcoming-trips-box">
                         <Link to="/upcomingtrips">Upcoming Trips</Link>
+                        <UpcomingTrips
+                        trips = {upcomingTrips} />
                     </div>
 
                     {/* Previous trips box */}
                     <div id="previous-trips-box">
                         <Link to="/previoustrips">Previous Trips</Link>
+                        <PreviousTrips
+                        trips = {prevTrips} />
                     </div>
 
                     {/* Dream trips box */}
                     <div id="dream-trips-box">
                         <Link to="/dreamtrips">Dream Trips</Link>
+
                     </div>
 
                     <div id="trips-box">
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -10,6 +10,8 @@ export const GET_USER_TRIPS = gql`
         _id
         location
         journalEntry
+        startTripDate
+        endTripDate
       }
     }
   }
